80c2bf62fe1d2c50df03ad9a58ea9a64d9061469
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-10-31 12:25:37 +0530

    ADD UC 2 The flip passed through loop to count the heads and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash 
+H=0
+T=0
+for i in `seq 10`
+do
 
-flip=$(($RANDOM%2))
+	flip=$(($RANDOM%2))
 
-if [[ $flip == 1 ]]
-then
-	echo "HEADS"
-else
-	echo "TAILS"
-fi
+	if [[ $flip == 1 ]]
+	then
+		echo "HEADS"
+		((H++))
+	else
+		echo "TAILS"
+		((T++))
+	fi
+
+done
+echo $H
+echo $T
