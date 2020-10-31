b9fa2e34f313a3222643e2c8a14fb30a57c35c90
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-10-31 13:07:53 +0530

    REFACTOR TO SHOW IF ITS WIN OR LIE AND BY HOW MUCH

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 H=0
 T=0
-for i in `seq 10`
+for i in `seq 45`
 do
 
 	flip=$(($RANDOM%2))
@@ -18,3 +18,21 @@ do
 done
 echo $H
 echo $T
+
+if [[ $H -gt $T ]]
+then
+	echo "HEAD WINS"
+	foldername=$(($H-$T))
+	echo "HEADS WON BY - " $foldername "TIMES"
+
+
+
+elif [[ $H -lt $T ]]
+then
+	echo "TAIL WINs"
+	folder=$(($T-$H))
+	echo "TAILS WON BY - " $folder "TIMES"
+
+else 
+	echo "ITS a TIE"
+fi
